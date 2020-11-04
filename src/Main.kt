311907ef4969c836ab907ced911ab6704ec5dbd7
DavidRiad <davidmagdynawar@gmail.com>
2020-11-04 12:21:24 +0100

    nullability check example

--- src/Main.kt
+++ src/Main.kt
@@ -12,7 +12,23 @@ println(nonNullableString)
     val length = nonNullableString.length
     println(length)
 
-    val nullLength =  nullableString?.length
+    // nullability check
+    var nullLength =  if (nullableString!= null) nullableString.length else -1
     println(nullLength)
+
+
+    //use let after checking the value of the item, if it's not null print it otherwise skip
+    val listWithNulls: List<String?> = listOf("Kotlin", null, "Java", "React")
+    for (item in listWithNulls) {
+        item?.let { println(it) } // prints every item and ignores null
+    }
+
+    //Elvis operator
+     nullLength =  nullableString?.length?: -1
+    println(nullLength)
+
+    //!! operator used to convert any value to a non-null type and throw exception if the value is null
+    val l = nullableString!!.length
+    println(l)
 
 }
