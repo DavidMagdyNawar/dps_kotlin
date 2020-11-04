15a1da80a032125ccf4d69b0a46fa0b16aabe252
DavidRiad <davidmagdynawar@gmail.com>
2020-11-04 12:11:05 +0100

    null safety example

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+var nonNullableString :String = "DPS"
+//    nonNullableString = null
+println(nonNullableString)
+
+    var nullableString :String? = "Kotlin"
+    nullableString = null
+    println(nullableString)
+
+
+    val length = nonNullableString.length
+    println(length)
+
+    val nullLength =  nullableString?.length
+    println(nullLength)
+
 }
